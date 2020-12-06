b047206312c6ca8610483e709fcc210b3009702f
error <bingyang038@gmail.com>
2020-12-06 12:28:32 -0500

    Revert "nothing change"
    This reverts commit 0f09344bd85a38e1ac971e008e4d9eced9daede5.
    
    something happen, so revert
    :x

--- self/mysqli.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$mysql = mysqli_connect($servername,$username,$password,$database);
-
-if($mysql -> connect_error){
-    die("connect failed " . $mysql->connect_error);
-}
-?>
--- self/selfCheckPoint.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-
-
-?>
-<h1>你好</h1>
-
-<a href="nihao.vbs" download="nihao.vbs">点击下载</a>
